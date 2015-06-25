f3346809c8a3636f76db9b424a73747cd9c459c3
Minos Galanakis <minos197@gmail.com>
2015-06-25 15:56:37 +0100

    piblinker.py: Functionality updated to include raw i2c communication

--- piblinker.py
+++ piblinker.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
-"""PiBlinker.py: A small library that uses wiriping pi access to raspbery pi GPIO 
+"""blinky.py: A small library that uses wiriping pi access to raspbery pi GPIO 
    ports,aimed at providing a simple notification interface"""
 
 __author__  = "minos197@gmail.com"
 __license__ = "LGPL"
 __version__ = "0.0.1"
 __email__   = "Minos Galanakis"
-__project__ = "codename"
+__project__ = "smartpi"
 __date__    = "01-06-2015"
 
 import sys
+import io
 import os
 import time
+import fcntl
+import struct
 import subprocess
 
 
@@ -23,6 +26,7 @@ class PiBlinker():
         self.LEDS = {"RED": 21, "GREEN": 26, "BLUE": 20}
         self.last_mode = 0
         self.setup()
+        self.i2c_devices = {}
 
     def run(self, cmd):
         """ Execute shell command in detached mdoe."""
@@ -87,9 +91,70 @@ class PiBlinker():
         print"|%s|> %s" % (led, text)
         self.blink(led, blink_no, 0.5)
 
+    def i2c_open_file(self, slave_id, bus = 1):
+        """Open the I2C channel for raw byte comms"""
+
+        if slave_id in self.i2c_devices.keys():
+            print "Device %d already open"%slave_id
+            return
+
+        #Open the file descriptors
+        read_ch  = io.open("/dev/i2c-"+str(bus), "rb", buffering=0)
+        write_ch = io.open("/dev/i2c-"+str(bus), "wb", buffering=0)
+
+        #Set the register
+        fcntl.ioctl(read_ch, 0x0703, slave_id)
+        fcntl.ioctl(write_ch, 0x0703, slave_id)
+
+        #store it to an internal dict
+        self.i2c_devices[slave_id] = (read_ch,write_ch)
+        #return the file descriptors if the user wants to manually drive them
+        return (read_ch,write_ch) 
+
+    def i2c_write_as(self, slave_id, format, data):
+        """Format data before i2c transmit using struct pack,"""
+
+        try:
+            wb_file = self.i2c_devices[slave_id][1]
+            wb_file.write(struct.pack(format,data))
+        except KeyError:
+            print "Device %d does not exit"%slave_id
+        except struct.error:
+            print "Pack Error make sure the data fits the format structure"
+        except:
+            print "Unspecified Error"
+
+    def i2c_read_as(self, slave_id, format, byte_no):
+        """Format data afte i2c rx using struct pack,"""
+        try:
+            rb_file = self.i2c_devices[slave_id][0]
+            return struct.unpack(format,rb_file.read(byte_no))
+        except KeyError:
+            print "Device %d does not exit"%slave_id
+        except struct.error:
+            print "Pack Error make sure the data fits the format structure"
+        except:
+            print "Unspecified Error"
+
+    def i2c_close(self,slave_id):
+        """Close the file descriptors associated to the slave channel"""
+
+        try:
+            self.i2c_devices.pop(slave_id)
+        except KeyError:
+            print "Device %d does not exit"%slave_id
+        
 if __name__ == "__main__":
     #Use case example/test
     pb = PiBlinker()
     pb.led_print("RED", "This is important")
     pb.led_print("GREEN", "This worked")
     pb.led_print("BLUE", "This you should know")
+
+    #Test the i2c
+    readf,writef = pb.i2c_open_file(0x04,1)
+    #read two bytes using the direct file descriptor
+    print repr(readf.read(2))
+
+    #read a 2byte uint8_t variable
+    print pb.i2c_read_as(04,">H",2)
