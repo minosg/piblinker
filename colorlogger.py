97b08e1659d061c0e510e59b019cf89641e0a2d2
Minos Galanakis <minos197@gmail.com>
2016-03-17 16:11:14 +0000

    Added colorlogging module, modified led_blink to utilize decorators

---
+++ colorlogger.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+
+"""colorlogger.py: New python logger with color output support."""
+
+__author__ = "Minoas Galanakis"
+__version__ = "0.0.1"
+__email__ = "minos197@gmail.com"
+__date__ = "04-03-2016"
+
+import re
+from datetime import datetime
+from inspect import getframeinfo, stack
+
+
+def colorlogger(logtype):
+    """Decorator that allows custom methods for clogger."""
+
+    def clog_decorator(func):
+        def clog_wrapper(self, *args):
+            return CLogger._color_stdout(func(self, *args), logtype)
+        return clog_wrapper
+    return clog_decorator
+
+
+class ANSIColors(object):
+
+    @classmethod
+    def setup(self):
+        """ Create class attributes for common ANSI colors"""
+
+        colors = ["BLACK", "RED", "GREEN", "YELLOW",
+                  "BLUE", "MAGENDA", "CYAN", "WHITE"]
+        normal = range(30, 38)
+        bright = range(90, 98)
+
+        a = [["%s" % c, "\033[%sm" % v] for c, v in zip(colors, normal)] +\
+            [["H%s" % c, "\033[%sm" % v] for c, v
+                in zip(colors, bright)] +\
+            [["U%s" % c, "\033[%s;4m" % v] for c, v
+                in zip(colors, normal)] +\
+            [["UH%s" % c, "\033[%s;4m" % v] for c, v
+                in zip(colors, bright)] +\
+            [["END", "\033[0m"]] +\
+            [["UEND", "\033[;21m"]]
+
+        self.color_list = ["%s%s" % (p, c) for c in colors
+                           for p in["", "H", "U", "UH"]]
+        for attrib in a:
+            setattr(self, *attrib)
+        return ANSIColors.cprint
+
+    @classmethod
+    def cprint(self, lne, sep=" "):
+        """Color print to terminal using ANSI colors.
+
+        Expects a list of inputs of color followed by text. Enables
+        users customization on value separations using optional sep
+        parameter. Can be used in both cprint["RED",
+        "Error","GREEN","Some Text"] or cprint([ANSIColors.RED,
+        "Error",ANSIColors.GREEN,"Some Text"]) Raises Arrgument Error
+        when color not found
+
+        """
+
+        colors = lne[::2]
+        # Do a sanity check on input and allow dual mode input
+        test_input = re.compile(r'^\033\[[3/9]{1}[0-9]{1}(?:;4)?m{1}$')
+        test_underline = re.compile(r'^\033\[[3/9]{1}[0-9]{1}(?:;4){1}m{1}.+$')
+        if not filter(test_input.findall, colors):
+            for i in range(len(colors)):
+                exec("colors[%d] = ANSIColors.%s" % (i, colors[i]))
+        cline = ["%s%s" % (x) for x in zip(colors, lne[1:][::2])]
+
+        # Compose the string
+        pline = ""
+        for c in cline:
+            pline += c
+            # If the line contains a underline it needs to be terminated
+            if test_underline.findall(c):
+                pline += ANSIColors.UEND
+            pline += sep
+        print pline + ANSIColors.END
+
+
+class CLogger(object):
+
+    def __init__(self):
+        raise ValueError('Clogger is not meant to be instantiated.\n'
+                         'Use as class with CLogger.setup(cmd, debug_level,'
+                         'file_path, colorPattern).\n'
+                         'Only cmd is required\n'
+                         'For example:\n'
+                         'logger = CLogger.setup("main_program","Debug",'
+                         '"/var/log/main.log",["WHITE","UWHITE"])')
+
+    @classmethod
+    def setup(self,
+              label="",
+              level="info",
+              path="",
+              alt_levels=None,
+              pattern=None):
+
+        self.LOG_PATH = path
+        self.LOG_LABEL = label
+        self.cprint = ANSIColors.setup()
+        self.set_def_colors(alt_levels)
+        self.set_level(level)
+        return self
+
+    @classmethod
+    def set_def_colors(self, color_dict=None):
+        """ Set the default colors for different levels of debuggin """
+
+        # Create the default dictionary
+        self.DLEVELS = {"info": [0, "BLUE"],
+                        "warning": [1, "YELLOW"],
+                        "error": [2, "HRED"],
+                        "debug": [3, "WHITE"],
+                        "ver_debug": [4, "WHITE"]}
+        # Extract the log base collor if it exists
+        try:
+            c = color_dict.pop("base_color")
+        except (KeyError, AttributeError):
+            c = "CYAN"
+        self.BASE_COLOR = ["%s%s" % (pre, c) for pre in ["", "U"]]
+
+        if isinstance(color_dict, dict):
+            # Update only the valid entries
+            self.DLEVELS.update({k: [v[0], color_dict[k]] for k, v
+                                in self.DLEVELS.iteritems()
+                                if k in color_dict and color_dict[k]
+                                in ANSIColors.color_list})
+
+    @classmethod
+    def set_level(self, lv="info"):
+        """ Set Logging level """
+
+        if not isinstance(lv, int):
+            try:
+                lv = self.DLEVELS[lv.lower()][0]
+            except KeyError:
+                raise ValueError("Error, supported debug levels"
+                                 "are: %s" % ", ".join(self.DLEVELS.keys()))
+        self.LEVEL = lv
+
+    @classmethod
+    def _log(self, msg, *args):
+
+        msg = [datetime.now().strftime('%F %T'),
+               "%s" % self.LOG_LABEL, (msg % args)]
+        # To file
+        if self.LOG_PATH:
+            try:
+                fp = open(self.LOG_PATH, 'a')
+                fp.write(" ".join(msg) + '\n')
+            except:
+                pass
+        return msg
+
+    @classmethod
+    def _color_stdout(self, dataset, ltype):
+        """ Print text with preformated color for debug level """
+
+        lv_no, lv_clr = self.DLEVELS[ltype]
+        if (self.LEVEL < lv_no):
+            return
+
+        pattern = self.BASE_COLOR + [lv_clr]
+        # Compose the line
+        colored_line = [
+            item for pair in zip(
+                pattern,
+                dataset) for item in pair]
+        # Add log type information and padding
+        padding = " " * (7 - len(ltype))
+
+        colored_line[-1] = "[%s]:%s %s" % (ltype, padding, colored_line[-1])
+
+        if self.LEVEL == self.DLEVELS["ver_debug"][0]:
+
+            mod, line, fn, _, _ = getframeinfo(stack()[-2][0])
+            details = " >> Called by %s() at %d in %s" % (fn, line, mod)
+            colored_line[-1] = colored_line[-1] + details
+
+        self.cprint(colored_line)
+
+    @classmethod
+    @colorlogger("error")
+    def error(self, msg, *args):
+        return self._log(msg, *args)
+
+    @classmethod
+    @colorlogger("info")
+    def info(self, msg, *args):
+        return self._log(msg, *args)
+
+    @classmethod
+    @colorlogger("warning")
+    def warning(self, msg, *args):
+        return self._log(msg, *args)
+
+    @classmethod
+    @colorlogger("debug")
+    def debug(self, msg, *args):
+        return self._log(msg, *args)
+
+
+if __name__ == '__main__':
+    log = CLogger.setup("colortest", "DEBUG")
+
+    log.info("This is an info")
+    log.warning("This is a warning")
+    log.error("This is an error")
+    log.debug("This is debug")
--- piblinker.py
+++ piblinker.py
@@ -3,35 +3,38 @@
 """blinky.py: A small library that uses wiriping pi access to raspbery pi GPIO
    ports,aimed at providing a simple notification interface"""
 
-__author__  = "minos197@gmail.com"
+__author__ = "minos197@gmail.com"
 __license__ = "LGPL"
 __version__ = "0.0.1"
-__email__   = "Minos Galanakis"
+__email__ = "Minos Galanakis"
 __project__ = "smartpi"
-__date__    = "01-06-2015"
+__date__ = "01-06-2015"
 
 import sys
 import io
-import os
 import time
 import fcntl
 import serial
 import struct
 from subprocess import Popen, PIPE
+from colorlogger import CLogger
 
 
-def colorprint(color):
-    """ Decorator that allows custom methods for clogger """
+def blinker(color, period=0.2, times=3):
+    """ Decorator that allows modular output formating for PiLogger """
 
-    def led_print_decorator(func):
-        def led_print_wrapper(*args):
-            class_obj, message =  args
-            return class_obj.led_print(color, func(class_obj, message))
-        return led_print_wrapper
-    return led_print_decorator
+    def blinker_decorator(func):
+        def func_wrapper(class_obj, message):
+            # Blinke the LED before printing sdout
+            class_obj.blink(color, times, period)
+            return func(class_obj, color, message)
+        return func_wrapper
+    return blinker_decorator
+
 
 class PiBlinkerError(Exception):
     __module__ = 'exceptions'
+
 
 class PiBlinker():
 
@@ -39,16 +42,33 @@ class PiBlinker():
         raise ValueError('PiBlinker is not meant to be instantiated')
 
     @classmethod
-    def setup(self):
+    def setup(self,
+              log_level="ver_debug",
+              log_label="PiBlinker",
+              log_path=None,
+              log_colors=None):
+
         """ Module Init."""
         # Map a color to GPIO.BCM PIN
         self.LEDS = {"RED": 17, "GREEN": 18, "BLUE": 27}
         self.last_mode = 0
         # Configure the GPIO ports in hardware
-        map(self.run, [ (x % n) for n in self.LEDS.values()
-                        for x in ["gpio export %d out",
-                                  "gpio -g mode %d out"]])
+        map(self.run, [(x % n) for n in self.LEDS.values()
+                       for x in ["gpio export %d out",
+                                 "gpio -g mode %d out"]])
         self.i2c_devices = {}
+
+        # Assosiate log levels with colors
+        if not log_colors:
+            log_colors = {"base_color": "CYAN",
+                          "info": "HBLUE",
+                          "warning": "RED",
+                          "error": "RED",
+                          "debug": "GREEN",
+                          "ver_debug": "GREEN"}
+
+        # Initalise the logging module
+        CLogger.setup(log_label, log_level, log_path, log_colors)
         return self
 
     @staticmethod
@@ -59,7 +79,7 @@ class PiBlinker():
         if err:
             # ignore warnings in error stream
             if "Warning" in err:
-                log.warning(err.strip())
+                CLogger.warning(err.strip())
                 return err
             raise PiBlinkerError(err)
         else:
@@ -89,7 +109,9 @@ class PiBlinker():
     def blink(self, led, times, delay=1):
         """ Blink an LED n number of times."""
 
-        if led not in ["RED", "GREEN", "BLUE"]:
+        # Make sure led is uppercase
+        led = led.upper()
+        if led not in self.LEDS.keys():
             return
         mode = 0
         count = 0
@@ -100,37 +122,69 @@ class PiBlinker():
             count += 1
 
     @classmethod
-    def led_print(self, led, text, blink_no=3):
+    def led_print(self, color, text):
         """ Print a debug message and notify the user with the LED."""
 
-        if led not in ["RED", "GREEN", "BLUE"]:
-            print "invalid led", led
-            return
-        print"|%s|> %s" % (led, text)
-        self.blink(led, blink_no, 0.5)
-
+        eval("self.%s" % color.lower())(text)
 
     @classmethod
-    @colorprint("RED")
-    def red(self, message):
-        return message
+    @blinker("RED")
+    def red(self, *args):
+        """ Print a debug message and notify the user with the LED."""
+
+        color, message = args
+        print"|%s|> %s" % (color, message)
 
     @classmethod
-    @colorprint("GREEN")
-    def green(self, message):
-        return message
+    @blinker("GREEN")
+    def green(self, *args):
+        """ Print a debug message and notify the user with the LED."""
+
+        color, message = args
+        print"|%s|> %s" % (color, message)
 
     @classmethod
-    @colorprint("BLUE")
-    def blue(self, message):
-        return message
+    @blinker("BLUE")
+    def blue(self, *args):
+        """ Print a debug message and notify the user with the LED."""
+
+        color, message = args
+        print"|%s|> %s" % (color, message)
 
     @classmethod
-    def uart_open(self, port = "/dev/ttyAMA0", baud = 9600, time_out = None):
+    @blinker("RED")
+    def error(self, *args):
+        """ Print a debug message and notify the user with the LED."""
+
+        CLogger.error(args[-1])
+
+    @classmethod
+    @blinker("BLUE")
+    def info(self, *args):
+        """ Print a debug message and notify the user with the LED."""
+
+        CLogger.info(args[-1])
+
+    @classmethod
+    @blinker("RED")
+    def warning(self, *args):
+        """ Print a debug message and notify the user with the LED."""
+
+        CLogger.warning(args[-1])
+
+    @classmethod
+    @blinker("GREEN")
+    def debug(self, *args):
+        """ Print a debug message and notify the user with the LED."""
+
+        CLogger.debug(args[-1])
+
+    @classmethod
+    def uart_open(self, port="/dev/ttyAMA0", baud=9600, time_out=None):
         """Open the Serial Channel"""
 
         try:
-            self.uart = serial.Serial(port, baud, timeout = time_out)
+            self.uart = serial.Serial(port, baud, timeout=time_out)
         except serial.SerialException:
             print "** Failed to initialize serial, check your port.** "
             raise ValueError
@@ -141,17 +195,19 @@ class PiBlinker():
 
         self.uart_open()
         countr = 0
-        #Test with a not supported command
+        # Test with a not supported command
         t_char = "O"
         while True:
             self.uart.write(t_char)
             if self.uart.inWaiting():
                 repl = self.uart.read(2)
-                if repl == "OK": print "UART Activated"
-                else: print "UART was already enabled"
+                if repl == "OK":
+                    print "UART Activated"
+                else:
+                    print "UART was already enabled"
                 break
             elif countr == 99:
-                #Test with a supported command to see if activated
+                # Test with a supported command to see if activated
                 t_char = "2"
             elif countr > 100:
                 break
@@ -160,10 +216,10 @@ class PiBlinker():
             countr += 1
 
     @classmethod
-    def uart_read(self, target = "ADC"):
+    def uart_read(self, target="ADC"):
         """Read the register through uart"""
 
-        cmd = { "ADC":"2", "PIN":"1" }
+        cmd = {"ADC": "2", "PIN": "1"}
 
         if target in cmd.keys():
             self.uart.write(cmd[target])
@@ -175,35 +231,36 @@ class PiBlinker():
         self.uart.close()
 
     @classmethod
-    def i2c_open_file(self, slave_id, bus = 1):
+    def i2c_open_file(self, slave_id, bus=1):
         """Open the I2C channel for raw byte comms"""
 
         if slave_id in self.i2c_devices.keys():
-            print "Device %d already open"%slave_id
+            print "Device %d already open" % slave_id
             return
 
-        #Open the file descriptors
-        read_ch  = io.open("/dev/i2c-"+str(bus), "rb", buffering=0)
+        # Open the file descriptors
+        read_ch = io.open("/dev/i2c-"+str(bus), "rb", buffering=0)
         write_ch = io.open("/dev/i2c-"+str(bus), "wb", buffering=0)
 
-        #Set the register
+        # Set the register
         fcntl.ioctl(read_ch, 0x0703, slave_id)
         fcntl.ioctl(write_ch, 0x0703, slave_id)
 
-        #store it to an internal dict
-        self.i2c_devices[slave_id] = (read_ch,write_ch)
-        #return the file descriptors if the user wants to manually drive them
-        return (read_ch,write_ch)
+        # store it to an internal dict
+        self.i2c_devices[slave_id] = (read_ch, write_ch)
+        # return the file descriptors if the user wants to manually drive them
+        return (read_ch, write_ch)
 
     @classmethod
     def i2c_write_as(self, slave_id, format, data):
-        """Write the data formatted using struct pack,Format needs to be specified"""
+        """Write the data formatted using struct pack,
+        Format needs to be specified"""
 
         try:
             wb_file = self.i2c_devices[slave_id][1]
-            wb_file.write(struct.pack(format,data))
+            wb_file.write(struct.pack(format, data))
         except KeyError:
-            print "Device %d does not exist"%slave_id
+            print "Device %d does not exist" % slave_id
         except struct.error:
             print "Pack Error make sure the data fits the format structure"
         except:
@@ -213,38 +270,38 @@ class PiBlinker():
     def i2c_read_as(self, slave_id, format, byte_no):
         try:
             rb_file = self.i2c_devices[slave_id][0]
-            return struct.unpack(format,rb_file.read(byte_no))
+            return struct.unpack(format, rb_file.read(byte_no))
         except KeyError:
-            print "Device %d does not exit"%slave_id
+            print "Device %d does not exit" % slave_id
         except struct.error:
             print "Pack Error make sure the data fits the format structure"
         except:
             raise IOError
 
     @classmethod
-    def i2c_close(self,slave_id):
+    def i2c_close(self, slave_id):
         """Close the file descriptors associated to the slave channel"""
         try:
             self.i2c_devices.pop(slave_id)
         except KeyError:
-            print "Device %d does not exit"%slave_id
+            print "Device %d does not exit" % slave_id
 
     @classmethod
-    def demux(self,data):
+    def demux(self, data):
         """ For efficiency purposes 10Bit ADC are muxed GPIO state."""
 
         adc_val = data & 0x3FF
         pin_val = (data >> 15)
-        return (adc_val,pin_val)
+        return (adc_val, pin_val)
 
     @classmethod
-    def i2c_read_adc(self,slave_id):
+    def i2c_read_adc(self, slave_id):
         """Reads data as returned from a 10Bit ADC sampling operation"""
 
         return self.demux(self.i2c_read_as(slave_id, '>H', 2)[0])[0]
 
     @classmethod
-    def i2c_read_pin(self,slave_id):
+    def i2c_read_pin(self, slave_id):
         """Reads data as returned from a 10Bit ADC sampling operation"""
 
         return self.demux(self.i2c_read_as(slave_id, '>H', 2)[0])[1]
@@ -256,14 +313,15 @@ class PiBlinker():
         detected = False
 
         try:
-            self.uart_open(time_out = 2)
+            self.uart_open(time_out=2)
             reading = self.uart_read("ADC")
-            if len(reading): detected = True
+            if len(reading):
+                detected = True
         except:
             pass
         try:
-            readf,writef = self.i2c_open_file(0x04,1)
-            self.i2c_read_as(04,">H",2)
+            readf, writef = self.i2c_open_file(0x04, 1)
+            self.i2c_read_as(04, ">H", 2)
             self.i2c_close(0x04)
             detected = True
         except:
@@ -272,7 +330,6 @@ class PiBlinker():
 
 if __name__ == "__main__":
     mode = 0
-    import serial
     pb = PiBlinker.setup()
 
     if len(sys.argv) == 3:
@@ -285,32 +342,32 @@ if __name__ == "__main__":
                 pb.green("This worked")
                 pb.blue("This you should know")
 
-                readf,writef = pb.i2c_open_file(0x04,1)
-                #read two bytes using the direct file descriptor
+                readf, writef = pb.i2c_open_file(0x04, 1)
+                # read two bytes using the direct file descriptor
                 print repr(readf.read(2))
 
-                #read a 2byte uint8_t variable
-                print pb.i2c_read_as(04,">H",2)
+                # read a 2byte uint8_t variable
+                print pb.i2c_read_as(04, ">H", 2)
                 pb.i2c_close(0x04)
 
             elif sys.argv[2] == "i2c":
-                readf,writef = pb.i2c_open_file(0x04, 1)
-                #read two bytes using the direct file descriptor
+                readf, writef = pb.i2c_open_file(0x04, 1)
 
-                print "2",repr(readf.read(2))
+                # read two bytes using the direct file descriptor
+                print "2", repr(readf.read(2))
 
-                #read a 2byte uint8_t variable
-                print pb.i2c_read_as(04,">H",2)
+                # read a 2byte uint8_t variable
+                print pb.i2c_read_as(04, ">H", 2)
                 pb.i2c_close(0x04)
 
             elif sys.argv[2] == "poll":
 
-                readf,writef = pb.i2c_open_file(0x04,1)
+                readf, writef = pb.i2c_open_file(0x04, 1)
                 while True:
 
-                    #Read using read ADC
-                    print "| ADC:",pb.i2c_read_adc(0x04),"| PIN: ",\
-                        pb.i2c_read_pin(0x04),"|"
+                    # Read using read ADC
+                    print "| ADC:", pb.i2c_read_adc(0x04), "| PIN: ",\
+                        pb.i2c_read_pin(0x04), "|"
                     time.sleep(0.2)
                 pb.i2c_close(0x04)
 
@@ -321,21 +378,28 @@ if __name__ == "__main__":
                 pb.uart_close()
 
             elif sys.argv[2] == "led":
-                pb.red("This is important")
-                pb.green("This worked")
-                pb.blue("This you should know")
+                pb.led_print("RED", "This is important")
+                pb.led_print("GREEN", "This is important2")
+                pb.led_print("BLUE", "This is important3")
 
-    elif  len(sys.argv) == 2 and sys.argv[1] == "-h":
+            elif sys.argv[2] == "log":
+                pb.info("This is an info")
+                pb.warning("This is a warning")
+                pb.error("This is an error")
+                pb.debug("This is debug")
+
+    elif len(sys.argv) == 2 and sys.argv[1] == "-h":
         print "\n ******** Basic testing commands ************"
         print "piblinker -t all: Test i2c communications and led"
         print "piblinker -t led: Test led"
+        print "piblinker -t log: Test led and loging output"
         print "piblinker -t i2c: Test i2c comms"
         print "piblinker -t poll: Continously poll ADC Switch readouts"
         print "piblinker -t uart: Get serial readouts"
 
         print "piblinker -a: Activate UART mode after a reset"
 
-    elif  len(sys.argv) == 2 and sys.argv[1] == "-a":
+    elif len(sys.argv) == 2 and sys.argv[1] == "-a":
         pb.uart_activate()
     else:
         print "use -h to see test command syntax"
